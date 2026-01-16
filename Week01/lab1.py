0f9c29e6ffc96e5939589511090321878b197b4d
jenifaj8 <jenifaj8>
2026-01-16 13:45:00 -0500

    Completed lab 1

---
+++ Week01/lab1.py
@@ -0,0 +1,25 @@
+# Sample Coding Question 01 Week 01
+# Jenifa Joseph
+
+
+# Question 2: Defining an array (list)
+my_array = [1, 4, 7, 9]
+
+# Question 3: Order of operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully bracketed version of e = a - b ** c // d + a % c
+e = (a - ((b ** c) // d)) + (a % c)
+print("The bracketed result is:", e)
+
+# Question 4: Formatting
+temperature = 32.6
+print("The temperature today is {:.1f} degrees Celsius.".format(temperature))
+
+# Question 5: Common Functions - User Input 
+UserAge = int(input("Enter your age: "))
+UserAge = UserAge + 22
+print("Now showing the shop items filtered by age", UserAge)
