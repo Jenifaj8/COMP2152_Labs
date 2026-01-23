618aba73fd8ce8186d45df08ab3a20395ea73e7d
jenifaj8 <jenifaj8>
2026-01-23 11:32:11 -0500

    add w02 solution

--- Week02/Lab02.py
+++ Week02/Lab02.py
@@ -1,1 +1,10 @@
 # Solution for the Week 02 Lab
+choices =["Rock", "Paper", "Scissors"]
+
+playerChoice =input("Enter a number between 1 to 3 for the following choices: 1. Rock, 2. Paper, 3. Scissors: ")
+
+playerChoice = int(playerChoice)
+
+if playerChoice < 1 or playerChoice > 3:
+    print("Error: Choice should be between 1 and 3!")
+
